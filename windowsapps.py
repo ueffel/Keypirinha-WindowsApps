f58606d4df151fd01de0b1ea73d8e9dc757e44a3
Steffen Brüheim <ueffel@gmail.com>
2021-01-29 17:13:04 +0100

    Fixes #12
    * force the powershell to output utf8
    * don't go into text mode with subprocess.Popen (universal_newlines=False) because it will fail to decode the output
    * decode the output of the powershell command explicitly

--- windowsapps.py
+++ windowsapps.py
@@ -166,14 +166,14 @@ class WindowsApps(kp.Plugin):
         startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
         output, err = subprocess.Popen(["powershell.exe",
                                         "-noprofile",
-                                        "Get-AppxPackage | ConvertTo-Json"],
+                                        "chcp 65001 >$null; Get-AppxPackage | ConvertTo-Json"],
                                        stdout=subprocess.PIPE,
-                                       universal_newlines=True,
+                                       universal_newlines=False,
                                        shell=False,
                                        startupinfo=startupinfo).communicate()
 
         catalog = []
-        packages = json.loads(output)
+        packages = json.loads(output.decode("utf8"))
         for package in packages:
             try:
                 catalog.extend(self._create_catalog_item(package))
